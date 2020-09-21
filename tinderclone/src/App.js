938ccf385e033d13bd49abcf8a161e1f35ec7ec6
Aman <aman.tiwari77245@gmail.com>
2020-09-21 18:13:55 +0530

    I am doing my first github commit

---
+++ tinderclone/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import {PersonIcon} from '@material-ui/icons/Person';
+import Header from './header'
+
+import './App.css';
+
+function App() {
+  
+  return (
+    <div >
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ tinderclone/src/header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+function header() {
+   
+    return (
+        <div>
+           <h2 > i am header </h2>
+           <p>Hello</p>
+            
+        </div>
+    )
+}
+
+
+
+export default header
+
